84431a0644ac4d95e888ece639613ca77e3dcef2
Sebastian <sebastian.stephenson1@gmail.com>
2022-03-31 18:39:53 -0400

    Set the input field to empty when task is added

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
+// Defining different elements for the page
 const list = document.querySelector("#list"); //This is the div that the list will be added to. 
 const btn = document.querySelector("button");// This holds the add task button
 const ol = document.createElement("ol");// Creating a ordered list element
 const form = document.getElementById("form");// This holds the form to with the search field and button
 const input = document.getElementById("input-fld");// This is the input field where you add the task
 const removeBtn = document.getElementById("remove-btn");// This is the remove button that will remove all checked tasks
+//-------------------------------------------------------------------------------------------------------------------------
 
+localStorage.removeItem("Tasks"); //TODO: Continue to work on localStorage.
+// Adding a submit event listener on the search form on the page
 form.addEventListener("submit", function(event){
   event.preventDefault();
   
@@ -20,13 +24,19 @@ form.addEventListener("submit", function(event){
   li.appendChild(label);// Adding a label with the checkbox; in order to display the task that was written in the inout field
   ol.appendChild(li);// Adding the list element to the ordered list tag
   list.appendChild(ol);// Adding the ordered list tag to the div for displaying the whole thing
+  input.value = "";
+
+  //localStorage.setItem('Tasks', ol.innerHTML);
+  
   
 })// End of eventlistener for submitting a task to the list
 
 
 removeBtn.addEventListener("click", function(){
   const liTags = list.getElementsByTagName("li");// Returns an array like object all the li tags in the list div
+  console.log("Old list" + list.innerHTML);
 
+  //localStorage.removeItem('Tasks')
   /*The for loop goes through all the li tags in the list div*/
   for(let i = 0; i < liTags.length; i++){
 
@@ -35,12 +45,20 @@ removeBtn.addEventListener("click", function(){
     if(liTags[i].querySelector("input").checked == true){
       ol.removeChild(liTags[i]);//removing the list element
     }// End of the if statement
+    //localStorage.setItem('Tasks', ol.innerHTML);
 
   }// End of the for loop
+  console.log("new list" + list.innerHTML); 
+
   
 })// End of the eventListener that was on the remove button. When clicked the button removes an item on the list that is checked.
 
+ 
+// let saved = localStorage.getItem('Tasks');
 
+// if(saved){
+//   ol.innerHTML = localStorage.getItem('Tasks');
+// }
 
 
 
